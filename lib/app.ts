bf3715ffc98aea2395907c2cd44645d3e77f5272
Naveed-rana <rana.naveed812@gmail.com>
2018-09-17 00:41:28 +0500

    Working TDD first write test for App

--- lib/app.ts
+++ lib/app.ts
@@ -1,11 +1,22 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as mongoose from "mongoose";
+import * as session from 'express-session';
+import * as cookieparser from 'cookie-parser';
 
 const App = express();
+App.use(bodyParser.urlencoded({
+    extended: true
+  }));
+App.use(bodyParser.json());
+App.use(session({
+  secret: "@#^&$!#_)(@!#)**(@^%*&^*#${}|{@#$@#$(#@",
+  resave: true,
+  saveUninitialized: true
+}));
+App.use(cookieparser());
 
-App.get('/',(req,res)=>{
- res.send("Hello world");
-});
+
+
 
 export {App};
--- lib/server.ts
+++ lib/server.ts
@@ -2,5 +2,5 @@ import {App} from "./app";
 const PORT = process.env.PORT || 8080;
 
 App.listen(PORT, () => {
-  console.log("Express sereistening on port " + PORT);
+  console.log("Express running on port " + PORT);
 });
